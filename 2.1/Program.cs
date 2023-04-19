b83eaf91ab58f81fe4ba71129be5e24ed2ba4b00
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-04-19 11:19:31 +0500

    Add project files.

---
+++ 2.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace _2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Student student = new Student();
+            Student student1 = new Student();
+            Console.WriteLine($"ID: {student.Id}, FirstName: {student.FirstName}, LastName: {student.LastName}, Age: {student.Age}, Group: {student.Group}, Grade: {student.Grade}");
+            Console.Write("Enter grade which you want change : ");
+            int n = int.Parse(Console.ReadLine());
+            student1.SetExamResult(n);
+            Console.WriteLine($"ID: {student1.Id}, FirstName: {student1.FirstName}, LastName: {student1.LastName}, Age: {student1.Age}, Group: {student1.Group}, Grade: {student1.Grade}");
+            Console.WriteLine("====================================================");
+            Main(args);
+
+
+
+            /*  student1.Id = 17;
+              student1.FirstName = "John";
+              student1.LastName = "Johnson";
+              student1.Age = 30;
+              student1.Group = 17;
+              student1.Grade = 7;
+              Console.WriteLine("=============================");
+              Console.WriteLine(student.Id);
+              Console.WriteLine(student.FirstName);
+              Console.WriteLine(student.LastName);
+              Console.WriteLine(student.Age);
+              Console.WriteLine(student.Group);
+              Console.WriteLine(student.Grade);
+              Console.WriteLine("==============================");
+              Console.WriteLine(studentt.Id);
+              Console.WriteLine(studentt.FirstName);
+              Console.WriteLine(studentt.LastName);
+              Console.WriteLine(studentt.Age);
+              Console.WriteLine(studentt.Group);
+              Console.WriteLine(studentt.Grade);
+              Console.WriteLine("===============================");
+              Console.WriteLine(student1.Id);
+              Console.WriteLine(student1.FirstName);
+              Console.WriteLine(student1.LastName);
+              Console.WriteLine(student1.Age);
+              Console.WriteLine(student1.Group);
+              Console.WriteLine(student1.Grade);*/
+
+        }
+    }
+}
---
+++ 2.1/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    internal class Student
+    {
+
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get;private set; }
+        public int Age { get; private set; }
+        public int Group { get; private set; }
+        public int Grade { get; private set; }
+
+
+        public Student()
+        {
+            Id = 001;
+            FirstName = "Avazbek";
+            LastName = "Muqumjonov";
+            Age = 17;
+            Group = 2;
+            Grade = 5;
+        }
+
+
+        public Student(int id, string firstName, string lastName, int age, int group, int grade)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Group = group;
+            Grade = grade;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ID: {Id}, FirstName: {FirstName}, LastName: {LastName}, Age: {Age}, Group: {Group}");
+        }
+        public void SetExamResult(int n)
+        {
+
+                if (n > 100)
+                {
+
+                 Console.WriteLine("Enter correct grade!");
+
+                }
+                Grade = n;
+            
+        }
+    }
+}
+
